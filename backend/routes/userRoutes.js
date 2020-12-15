7f1b23434051d71b95359201be9a06e56f3191db
kaixuannnn <kaixuant@yahoo.com>
2020-12-15 21:46:26 +0800

    Passwrod Encrypt

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,13 @@ import express from 'express'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userController.js'
 
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
